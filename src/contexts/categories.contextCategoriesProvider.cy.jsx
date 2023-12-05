93850ef5b06f41620c03d996d85be717dd9d2515
Tiffany Chen <tiffanychen@Tiffanys-MBP.localdomain>
2023-12-05 16:45:53 +0800

    removed lazy loading

--- src/contexts/categories.contextCategoriesProvider.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { CategoriesProvider } from './categories.context'
-
-describe('<CategoriesProvider />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<CategoriesProvider />)
-  })
-})
