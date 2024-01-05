9b70e3d9f74166abf0177fd6ab0bfe02b38f6656
Tiffany Chen <tiffanychen@Tiffanys-MBP.localdomain>
2024-01-05 15:58:26 +0800

    adding PWA

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { CategoriesProvider } from './contexts/categories.context';
-
+import * as serviceWorkerResgistration from './serviceWorkerRegistration'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -17,7 +17,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+
+serviceWorkerResgistration.register()
